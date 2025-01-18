1041c248b618cee30b6370d8034a029e65fb4bf9
Alexandre Puech <alexandre.puech3@gmail.com>
2025-01-18 10:52:44 +0100

    feat: implemented fastest algo

---
+++ src/algo/algo-for-loop.js
@@ -0,0 +1,36 @@
+import { recipes } from "../data/recipe.js";
+import { normalizeString } from "./normalize-string.js";
+
+const userSearch = "Concombre";
+
+const normalizedUserSearch = normalizeString(userSearch);
+
+function filterRecipes(normalizedUserSearch, recipes) {
+  if (userSearch.length < 3) {
+    return;
+  }
+  const filteredRecipes = new Set();
+
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+
+    if (recipe.name.includes(normalizedUserSearch)) {
+      filteredRecipes.add(recipe);
+    }
+
+    if (recipe.description.includes(normalizedUserSearch)) {
+      filteredRecipes.add(recipe);
+    }
+
+    for (let j = 0; j < recipe.ingredients.length; j++) {
+      const ing = recipe.ingredients[j];
+      if (ing.ingredient.includes(normalizedUserSearch)) {
+        filteredRecipes.add(recipe);
+      }
+    }
+
+    return Array.from(filteredRecipes);
+  }
+}
+
+filterRecipes(normalizedUserSearch, recipes);
